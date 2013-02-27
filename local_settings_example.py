a12e00c9235e03d461aa45db1e837e6d875fa65f
Derek Dohler <derek@transparency.ge>
2013-02-27 13:46:34 +0400

    Added placeholder settings files

---
+++ local_settings_example.py
@@ -0,0 +1,23 @@
+DATABASE_ENGINE = 'postgresql_psycopg2'          # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'chemikucha'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'chemikucha'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'chemikucha'         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+#
+EMAIL_USE_TLS = ''
+EMAIL_HOST = 'smtp.mysite.com'
+EMAIL_HOST_USER = 'chemikucha'
+EMAIL_HOST_PASSWORD = 'chemikucha'
+EMAIL_PORT = '25'
+EMAIL_FROM_USER = 'system@mysite.com'
+DEBUG = True 
+LOCAL_DEV = 'http://127.0.0.1:8000'
+SITE_URL = 'http://fixmystreet.ge'
+SECRET_KEY= ''
+GMAP_KEY= ''
+SITE_ID= 0
+#
+ADMIN_EMAIL = 'chemikucha@chemikucha.ge'
+ADMINS = ['Chemi Kucha']
+
