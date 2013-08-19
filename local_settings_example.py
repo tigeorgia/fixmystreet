ad3072d2494be69cabf202e44a9a41cc34d1b8d5
George Lubaretsi <georgelubaretsi@gmail.com>
2013-08-19 16:05:18 +0400

    Changes to examples to comply with new versions

--- local_settings_example.py
+++ local_settings_example.py
@@ -1,10 +1,13 @@
-DATABASE_ENGINE = 'postgresql_psycopg2'          # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'chemikucha'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'chemikucha'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'chemikucha'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': ''
+    }
+}
+
+ALLOWED_HOSTS = ['mysite.com', 'www.mysite.com']
 EMAIL_USE_TLS = ''
 EMAIL_HOST = 'smtp.mysite.com'
 EMAIL_HOST_USER = 'chemikucha'
@@ -13,11 +16,13 @@ EMAIL_PORT = '25'
 EMAIL_FROM_USER = 'system@mysite.com'
 DEBUG = True 
 LOCAL_DEV = 'http://127.0.0.1:8000'
-SITE_URL = 'http://fixmystreet.ge'
-SECRET_KEY= ''
-GMAP_KEY= ''
+SITE_URL = 'http://mysite.com'
+# Google maps API key. Learn how to obtain here:
+# https://developers.google.com/maps/documentation/javascript/tutorial#api_key
+GMAP_KEY = ''
+GOOGLE_MAPS_API_VERSION = '2.0'
+# Set this or Django won't run
+SECRET_KEY = ''
 SITE_ID= 0
-#
-ADMIN_EMAIL = 'chemikucha@chemikucha.ge'
+ADMIN_EMAIL = 'chemikucha@mysite.com'
 ADMINS = ['Chemi Kucha']
-
