bdda64de0583030632346174854e806254375076
George Lubaretsi <georgelubaretsi@gmail.com>
2013-08-19 16:48:03 +0400

    New urls and code reformat

--- urls.py
+++ urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.contrib import admin
 from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
 from mainapp.feeds import LatestReports, LatestReportsByCity, LatestReportsByWard, LatestUpdatesByReport
-from mainapp.models import City
+from mainapp.models import City, Report, ReportFilter
 from mainapp.sitemaps import MainSitemap
 import mainapp.views.cities as cities
+
+js_info_dict = {
+    'packages': ('django-fixmystreet',),
+}
 
 feeds = {
     'reports': LatestReports,
@@ -20,87 +25,97 @@ sitemaps = {
     "flatpages": FlatPageSitemap
 }
 
-
 admin.autodiscover()
 urlpatterns = patterns('',
-    (r'^admin/(.*)', admin.site.root),
-    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
-    (r'^i18n/', include('django.conf.urls.i18n')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.Feed', {'feed_dict': feeds}),
+                       url(r'^i18n/', include('django.conf.urls.i18n')),
 )
 
 urlpatterns += patterns('',
-                    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
-)
-                        
-urlpatterns += patterns('mainapp.views.main',
-    (r'^$', 'home'),
-    (r'^search', 'search_address'),
-    (r'about/$', 'about')
-)
-
-urlpatterns += patterns('mainapp.views.faq',
-    (r'^about/(\S+)$', 'show'),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 )
 
 
-urlpatterns += patterns('mainapp.views.promotion',
-    (r'^promotions/(\w+)$', 'show'),
+urlpatterns += i18n_patterns('',
+                        url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
+)
+
+urlpatterns += i18n_patterns('mainapp.views.main',
+                        url(r'^$', 'home'),
+                        url(r'about/$', 'about')
+)
+
+urlpatterns += i18n_patterns('mainapp.views.faq',
+                        url(r'^about/(\S+)$', 'show'),
+)
+
+urlpatterns += i18n_patterns('mainapp.views.promotion',
+                        url(r'^promotions/(\w+)$', 'show'),
+)
+
+urlpatterns += i18n_patterns('mainapp.views.wards',
+                        url(r'^wards/(?P<ward_id>\d+)', 'show'),
+                        url(r'^cities/(?P<city_id>\d+)/wards/(?P<ward_id>\d+)', 'show_by_number'),
+
 )
 
 
-urlpatterns += patterns('mainapp.views.wards',
-    (r'^wards/(\d+)', 'show'),       
-    (r'^cities/(\d+)/wards/(\d+)', 'show_by_number'),       
-    
+urlpatterns += i18n_patterns('mainapp.views.reports.updates',
+                        (r'^reports/updates/confirm/(\S+)', 'confirm'),
+                        (r'^reports/updates/create/', 'create'),
+                        (r'^reports/(?P<report_id>\d+)/updates/', 'new'),
 )
 
-urlpatterns += patterns('',
-    (r'^cities/(\d+)$', cities.show ),       
-    (r'^cities/(\d+)/category/(\d+)$', cities.category),
-    (r'^cities', cities.index),
-
+urlpatterns += i18n_patterns('mainapp.views.reports.subscribers',
+                        (r'^reports/subscribers/confirm/(\S+)', 'confirm'),
+                        (r'^reports/subscribers/unsubscribe/(\S+)', 'unsubscribe'),
+                        (r'^reports/subscribers/create/', 'create'),
+                        (r'^reports/(?P<report_id>\d+)/subscribers', 'new'),
 )
 
-urlpatterns += patterns( 'mainapp.views.reports.updates',
-    (r'^reports/updates/confirm/(\S+)', 'confirm'), 
-    (r'^reports/updates/create/', 'create'), 
-    (r'^reports/(\d+)/updates/', 'new'),
+urlpatterns += i18n_patterns('mainapp.views.reports.flags',
+                        (r'^reports/(?P<report_id>\d+)/flags/thanks', 'thanks'),
+                        (r'^reports/(?P<report_id>\d+)/flags', 'new'),
 )
 
-
-urlpatterns += patterns( 'mainapp.views.reports.subscribers',
-    (r'^reports/subscribers/confirm/(\S+)', 'confirm'), 
-    (r'^reports/subscribers/unsubscribe/(\S+)', 'unsubscribe'),
-    (r'^reports/subscribers/create/', 'create'),
-    (r'^reports/(\d+)/subscribers', 'new'),
+urlpatterns += i18n_patterns('mainapp.views.reports.main',
+                        (r'^reports/(?P<report_id>\d+)$', 'show'),
+                        (r'^reports/(?P<report_id>\d+)/$', 'show'),
+                        (r'^reports/(?P<report_id>\d+)/poster$', 'poster'),
+                        #(r'^reports/category/(\d+)$', 'category'),
+                        (r'^reports/', 'new'),
 )
 
-urlpatterns += patterns( 'mainapp.views.reports.flags',
-    (r'^reports/(\d+)/flags/thanks', 'thanks'),
-    (r'^reports/(\d+)/flags', 'new'),
+urlpatterns += i18n_patterns('mainapp.views.contact',
+                        (r'^contact/thanks', 'thanks'),
+                        (r'^contact', 'new'),
 )
 
-urlpatterns += patterns('mainapp.views.reports.main',
-    (r'^reports/(\d+)$', 'show'),
-	(r'^reports/(\d+)/$', 'show'),
-    (r'^reports/(\d+)/poster$', 'poster'),
-    #(r'^reports/category/(\d+)$', 'category'),
-    (r'^reports/', 'new'),
+urlpatterns += i18n_patterns('mainapp.views.ajax',
+                        (r'^ajax/categories/(?P<cat_id>\d+)', 'category_desc'),
+                        (r'^ajax/address-search-form', 'address_search_form'),
+                        (r'^ajax/new-report$', 'new_report'),
 )
 
-urlpatterns += patterns('mainapp.views.contact',
-    (r'^contact/thanks', 'thanks'),
-    (r'^contact', 'new'),
-)
-
-urlpatterns += patterns('mainapp.views.ajax',
-    (r'^ajax/categories/(\d+)', 'category_desc'),
+urlpatterns += i18n_patterns('mainapp.views.reports',
+                             (r'^all-reports/', 'main.report_list'),
 )
 
 #The following is used to serve up local media files like images
 if settings.LOCAL_DEV:
     baseurlregex = r'^media/(?P<path>.*)$'
     urlpatterns += patterns('',
-        (baseurlregex, 'django.views.static.serve',
-        {'document_root':  settings.MEDIA_ROOT}),
+                            (baseurlregex, 'django.views.static.serve',
+                             {'document_root': settings.MEDIA_ROOT}),
+    )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+        url(r'^rosetta/', include('rosetta.urls')),
     )
