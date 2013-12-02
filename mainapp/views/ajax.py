b4d7e88cb10b9168f48bf842d93d4140bc4f2b7c
George Lubaretsi <gio@transparency.ge>
2013-12-02 17:01:34 +0400

    Json api to get latest 800 reports. Used for map markers

--- mainapp/views/ajax.py
+++ mainapp/views/ajax.py
@@ -1,16 +1,40 @@
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotAllowed
 from django.template import Context, RequestContext
+from django.views.generic.list import ListView
 from mainapp.forms import ReportStart
-from mainapp.models import ReportCategory, ReportCategoryClass
+from mainapp.models import Report, ReportCategory, ReportCategoryClass
 from django.utils.translation import ugettext as _
+import json
+
+
+class latestReportsJson(ListView):
+    model = Report
+
+
+
+
+def latest_reports_json(request):
+    reports = Report.objects.all().filter(is_confirmed=True).order_by('-created_at')[:800]
+
+    data = json.dumps([{'id': r.id,
+                        'author': r.author,
+                        'title': r.title,
+                        'created_at': r.created_at.isoformat(),
+                        'is_fixed': r.is_fixed,
+                        'description': r.desc[:300] + '...',
+                        'point': json.loads(r.point.json)} for r in reports], cls=DjangoJSONEncoder)
+    return HttpResponse(data, 'json')
+
 
 def category_desc(request, id):
     return render_to_response("ajax/category_description.html",
                 {"category": ReportCategory.objects.get(id=id),
                   },
                 context_instance=RequestContext(request))
-   
+
 def address_search_form(request):
     return render_to_response("ajax/address_geocode_form.html")
 
