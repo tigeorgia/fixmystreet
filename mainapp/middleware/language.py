d56672c34fce0c6aedc5979b644efd7e95163b8e
George Lubaretsi <gio@transparency.ge>
2013-12-02 16:48:50 +0400

    New urls to ignore

--- mainapp/middleware/language.py
+++ mainapp/middleware/language.py
@@ -5,18 +5,22 @@ from django.utils import translation
 
 
 class LanguagePrefixMiddleware:
+    def __init__(self):
+        self.lang_blacklist = ['i18n', 'jsi18n', 'media', 'admin', 'rosetta', 'accounts', 'static']
+        self.codes = []
+
     def process_request(self, request):
-        LANG_BLACKLIST = ['i18n', 'jsi18n', 'media', 'admin', 'rosetta']
+        #Get the path without language code
         l_path = request.path.split('/')
         request.session['path_without_lang'] = request.path
-        codes = []
+
         for code, name in LANGUAGES:
-            codes.append(code)
-        if l_path[1] in codes:
+            self.codes.append(code)
+        if l_path[1] in self.codes:
             del l_path[1]
             path_without_lang = '/'.join(l_path)
             request.session['path_without_lang'] = path_without_lang
-        elif l_path[1] not in codes and l_path[1] not in LANG_BLACKLIST:
+        elif l_path[1] not in self.codes and l_path[1] not in self.lang_blacklist:
             cur_lang = request.session.get('django_language', settings.LANGUAGE_CODE)
             path = ('/' + cur_lang + request.path)
             return HttpResponsePermanentRedirect(path)
