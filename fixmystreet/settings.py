64e1f251131f64d070c2fd94f545c27047bfff74
GeorgeLubaretsi <gio@transparency.ge>
2014-12-17 23:02:01 +0400

    Authentication classes for browsable api

--- fixmystreet/settings.py
+++ fixmystreet/settings.py
@@ -166,6 +166,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',
