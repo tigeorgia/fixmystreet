a4eaea959654789033620131cdb1d34200646d87
GeorgeLubaretsi <gio@transparency.ge>
2015-01-30 17:16:39 +0400

    Metadata for ReportCount API view

--- apps/api/metadata.py
+++ apps/api/metadata.py
@@ -1,6 +1,7 @@
 from rest_framework.metadata import SimpleMetadata, BaseMetadata
 from rest_framework.utils.field_mapping import ClassLookupDict
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 import fields
 
 
@@ -36,12 +37,34 @@ class AuthTokenMetaData(BaseMetadata):
                 'POST' : {
                     'email': {
                         'type': 'string',
-                        'help_text': 'User email'
+                        'help_text': _('Email')
                     },
                     'password': {
                         'type': 'string',
-                        'help_text': 'Password'
+                        'help_text': _('Password')
                     }
+                }
+            }
+        }
+
+class ReportCountMetadata(BaseMetadata):
+
+    def determine_metadata(self, request, view):
+        return {
+            'name': view.get_view_name(),
+            'description': view.get_view_description(),
+            'values': {
+                'total': {
+                    'description': _('Total number of reports')
+                },
+                'fixed': {
+                    'description': _("Fixed reports")
+                },
+                'not-fixed': {
+                    'description': _('Not fixed reports')
+                },
+                'in-progress': {
+                    'description': _('Reports in progress')
                 }
             }
         }
