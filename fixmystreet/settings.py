0464214306fc973e1a6c51682e1fc7fbfe9146ab
GeorgeLubaretsi <gio@transparency.ge>
2015-04-02 15:22:22 +0400

    Organized settings file

--- fixmystreet/settings.py
+++ fixmystreet/settings.py
@@ -3,45 +3,80 @@
 import os
 import logging
 
+
+######################################
+# ############ VARIABLES #############
+######################################
+
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 TEST_RUNNER = 'django.contrib.gis.tests.run_tests'
 POSTGIS_TEMPLATE = 'template_postgis'
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+######################################
+
+
+######################################
+# ############# LOCALE ###############
+######################################
+
 TIME_ZONE = 'Asia/Tbilisi'
-APPEND_SLASH = True
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'ka'
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
+DATE_FORMAT = "l, F jS, Y"
+LANGUAGES = (
+    ('en', 'English'),
+    ('ka', 'Georgian'),
+)
 
-# URL that handles the media and static served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-# Serve with nginx
+LOCALE_PATHS = (
+    os.path.join(PROJECT_PATH, 'locale'),
+)
+
+######################################
+
+
+######################################
+# ############ URL & PATH ############
+######################################
+
+APPEND_SLASH = True
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-
-RANDOM_IMAGE_DIR = 'images/header-images/'  # random home images
+RANDOM_IMAGE_DIR = 'images/header-images/'
 LOGIN_REDIRECT_URL = 'api:login-redirect'
+# PREPEND_WWW = True
+ADMIN_MEDIA_ROOT = 'django.contrib.admin.media'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-# GOOGLE MAPS SETTINGS
+######################################
+
+
+######################################
+# ############ GOOGLE MAPS ###########
+######################################
+
 GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/js?sensor=true&v=%s&key='
 GOOGLE_MAPS_API_VERSION = 3
 
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
+######################################
 
-# START Pipeline settings
+
+######################################
+# ############ STATICFILES ###########
+######################################
+
+FILE_UPLOAD_PERMISSIONS = 0644
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
+
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
-
 # If javascript breaks, set this to true
 PIPELINE_DISABLE_WRAPPER = True
 
@@ -120,21 +155,16 @@ PIPELINE_YUI_CSS_ARGUMENTS = ''
 PIPELINE_YUI_JS_ARGUMENTS = ''
 
 PIPELINE_ENABLED = True
-# END Pipeline settings
 
-# PREPEND_WWW = True
+######################################
 
-ADMIN_MEDIA_ROOT = 'django.contrib.admin.media'
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-FILE_UPLOAD_PERMISSIONS = 0644
-DATE_FORMAT = "l, F jS, Y"
-
-AUTH_USER_MODEL = 'users.FMSUser'
+######################################
+# ############# TEMPLATE #############
+######################################
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -152,6 +182,20 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "allauth.account.context_processors.account",
     "allauth.socialaccount.context_processors.socialaccount",
 )
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
+)
+
+######################################
+
+
+######################################
+# ############ MIDDLEWARE ############
+######################################
 
 MIDDLEWARE_CLASSES = (
     'apps.mainapp.middleware.webfaction.WebFactionFixes',
@@ -173,14 +217,12 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
-)
+######################################
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
-)
+
+######################################
+# ########## REST FRAMEWORK ##########
+######################################
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
@@ -202,21 +244,37 @@ REST_FRAMEWORK = {
     'DEFAULT_MODEL_SERIALIZER_CLASS': 'rest_framework.serializers.ModelSerializer'
 }
 
-LANGUAGES = (
-    ('en', 'English'),
-    ('ka', 'Georgian'),
+######################################
+
+
+######################################
+# ############## AUTH ################
+######################################
+
+AUTH_USER_MODEL = 'users.FMSUser'
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-LOCALE_PATHS = (
-    os.path.join(PROJECT_PATH, 'locale'),
-)
+SOCIALACCOUNT_PROVIDERS = \
+    {'facebook':
+         {'SCOPE': ['email', 'publish_stream'],
+          'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+          'METHOD': 'oauth2',
+          'LOCALE_FUNC': 'path.to.callable',
+          'VERIFIED_EMAIL': True,
+          'VERSION': 'v2.2'}
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'templates'),
-)
+    }
+
+######################################
+
+
+######################################
+# ############## APPS ################
+######################################
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -251,24 +309,19 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.facebook',
 )
 
-AUTHENTICATION_BACKENDS = (
-    "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
-)
+######################################
 
-SOCIALACCOUNT_PROVIDERS = \
-    {'facebook':
-         {'SCOPE': ['email', 'publish_stream'],
-          'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-          'METHOD': 'oauth2',
-          'LOCALE_FUNC': 'path.to.callable',
-          'VERIFIED_EMAIL': True,
-          'VERSION': 'v2.2'}
 
-    }
+######################################
+# ############ VARIOUS ###############
+######################################
 
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
 DEBUG = False
 LOCAL_DEV = False
+
+######################################
+
 
 try:
     from local_settings import *
